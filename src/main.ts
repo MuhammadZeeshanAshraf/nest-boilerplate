f476ccb9f55a804abe69ad8052eb5b1e37ec6b10
Zeeshan Ashraf <zeeshanashraf633b@gmail.com>
2023-06-28 14:43:18 +0500

    Added whitelisting and transform pipe for request Validation

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { getSwaggerConfiguration } from './swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -10,6 +11,12 @@ async function bootstrap() {
   });
 
   const configService: ConfigService = app.get(ConfigService);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
   await getSwaggerConfiguration(app);
   await app.listen(configService.get<number>('PORT'));
 }
