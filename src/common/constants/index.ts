cac3247c68094fc1e86e82f7325b0ca603db16a6
Zeeshan Ashraf <zeeshanashraf633b@gmail.com>
2023-06-28 18:59:18 +0500

    Throttle Environment Variable Min and Max Contraint Added

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -1,2 +1,17 @@
+/** */
 export const PROJECT_NAME = 'Nest Boilerplate 🚀';
 export const JWT = 'JWT';
+
+/** */
+export const THROTTLE = {
+    TTL: {
+        DEFAULT: 60,
+        MIN: 1,
+        MAX: 3600,
+    },
+    LIMIT: {
+        DEFAULT: 10,
+        MIN: 1,
+        MAX: 100,
+    }
+}
