18ee3fcbe810704aab86838e83d92d2796137842
Zeeshan Ashraf <zeeshanashraf633b@gmail.com>
2023-07-08 18:00:49 +0500

    ci: gobal try and catch handling integrated

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -1,6 +1,8 @@
 /** */
 export const PROJECT_NAME = 'Nest Boilerplate 🚀';
 export const JWT = 'JWT';
+export const PAGE_SIZE = 10;
+export const DEFLAUT_PAGE = 1;
 
 /** */
 export const THROTTLE = {
@@ -16,6 +18,16 @@ export const THROTTLE = {
     }
 }
 
+export const NODE_ENV = {
+    DEVELOPMENT: 'development',
+    PRODUCTION: 'production',
+};
+
 export const CRON_JOB_NAME = {
     CHECK_SERVER_HEALTH: 'CHECK_SERVER_HEALTH',
 }
+
+export const RESPONSE_MESSAGE = {
+    INTERNAL_SERVER_ERROR:
+        "We're sorry, but our server encountered an unexpected error while processing your request. Please try again later, or contact our support team if the problem persists. We apologize for any inconvenience this may have caused",
+};
